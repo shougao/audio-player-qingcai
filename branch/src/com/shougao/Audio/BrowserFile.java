c6751aef934af115388236b5123477159853a3cd
zhangqingcai0815@gmail.com <zhangqingcai0815@gmail.com@f776371a-a297-3d5d-be45-683a56f9ef12>
2011-11-16 09:59:40 +0000

    添加播放列表

--- branch/src/com/shougao/Audio/BrowserFile.java
+++ branch/src/com/shougao/Audio/BrowserFile.java
@@ -1,6 +1,9 @@
 package com.shougao.Audio;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -20,13 +23,15 @@ public class BrowserFile extends Activity{
 	
 	//定义各个变量
 	private EditText inputEditor;
-	private String getPath;
 	private Button getPathButton;
 	private Bundle b;
 	private Intent iBrowser;
 	private ListView dirListView;
 	private ArrayAdapter<String> dirAdapter = null;
 	private ArrayList<String> currentDirList = new ArrayList<String>();
+	private HashMap dirPathSet = new HashMap();
+	private String selectedPath = "/";
+	private String selectedDir = null;
 	
 	/*
 	 * 在oncreate中初始化各个按钮和list
@@ -37,52 +42,107 @@ public class BrowserFile extends Activity{
         super.onCreate(savedInstanceState);  
         setContentView(R.layout.browser);
 
+        updateDirList("/");
         dirListView = (ListView)findViewById(R.id.dirlist);
         dirListView.setOnItemClickListener(dirListOnItemClickListenerClick);
         getPathButton = (Button)findViewById(R.id.browserPlay); 
         inputEditor = (EditText)findViewById(R.id.dirPath);
-        getPath = inputEditor.getText().toString();
+        
         getPathButton.setOnClickListener(new OnClickListener(){
-
+        	
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				iBrowser = new Intent(BrowserFile.this, AudioActivity.class);
 				b = new Bundle();
-				b.putString("str", getPath);
+				b.putString("str", selectedPath);
 				iBrowser.putExtras(b);
 		        BrowserFile.this.setResult(RESULT_OK, iBrowser);  
 		        BrowserFile.this.finish();
 		        System.out.println("======browserPlayEnd");
 			}
         });
-        System.out.print("===========helo");
-        currentDirList.add("updir");
         dirAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, currentDirList);        
         dirListView.setAdapter(dirAdapter);
     }
     
     
-    OnItemClickListener dirListOnItemClickListenerClick = new OnItemClickListener(){
+	OnItemClickListener dirListOnItemClickListenerClick = new OnItemClickListener(){
 
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position,
 				long id) {
 			// TODO Auto-generated method stub
 			ListView listView = (ListView)parent;
-			String selectedPath = (String) listView.getItemAtPosition(position);
-			System.out.println("=="+selectedPath);
-			inputEditor.setText(selectedPath);
-			getPath = selectedPath;
-			setNewDirList(selectedPath);
+			selectedDir = (String) listView.getItemAtPosition(position);
+			String strPath =  (String)dirPathSet.get(selectedDir);
+//			if(!selectedPath.equals("/")){
+//				if(selectedDir.equals("..")){
+//					upToDir();
+//				}
+//			}else{
+//				goToDir(strPath);
+//			}
+			goToDir(strPath);
 		}
-
-		private void setNewDirList(String selectedPath) {
-			// TODO Auto-generated method stub
-			
-		}
-    	
     };
+    
+    /**
+     * 点击指定目录后，更新新的列表list
+     * @param path
+     */
+    private void updateDirList(String path) {
+		// TODO Auto-generated method stub
+		File f = new File(path);
+		if(!f.exists()){
+			return;
+		}
+		if(f.canRead()){
+			if(f.isDirectory()){
+				dirPathSet.clear();
+				currentDirList.clear();
+				String list[] = f.list();
+				for(int i=0; i<list.length; i++){
+					File checkIsDir = new File(f.getPath()+File.separator+list[i]);
+					if(checkIsDir.isDirectory()){
+						currentDirList.add(checkIsDir.getName());//只存放名字
+						dirPathSet.put(list[i], checkIsDir.getAbsolutePath());//名字和对应的path
+						System.out.println("==========get a dir:"+checkIsDir.getName());
+					}
+				}
+			}
+		}
+	}
+
+    protected void goToDir(String path) {
+		// TODO Auto-generated method stub
+		inputEditor.setText(path);
+		updateDirList(path);
+		updateDirAdapter();
+	}
+
+	/**
+     * 返回到上层目录
+     */
+    protected void upToDir() {
+		// TODO Auto-generated method stub
+		File path = new File(selectedPath);
+		String parentPath = path.getParentFile().getAbsolutePath();
+		goToDir(parentPath);
+	}
+
+	/**
+     * 根据新的目录列表，把新的列表显示到界面上
+     */
+	protected void updateDirAdapter() {
+		// TODO Auto-generated method stub
+		dirAdapter.clear();
+		dirAdapter.add("..");
+		for(String str: currentDirList){
+			dirAdapter.add(str);
+		}
+		dirAdapter.notifyDataSetChanged();
+	}
 } 
 
 
